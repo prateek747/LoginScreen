b6397629fb711e80b72ebf4eb7acf9ab397284c0
Prateek M <Pratoo@Maheshs-MacBook-Pro.local>
2016-08-21 09:36:12 +0530

    Initial commit

---
+++ LoginScreen.java
@@ -0,0 +1,18 @@
+package com.webs.buzztechies.loginscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Pratoo on 31/07/16.
+ */
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+
+    }
+}
